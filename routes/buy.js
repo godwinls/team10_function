23666f843c83be81263093e7c7d2506e5f47deab
godwinls <263209050@qq.com>
2014-12-05 19:55:26 -0800

    buy

--- routes/buy.js
+++ routes/buy.js
@@ -9,6 +9,8 @@ exports.buy = function(req,res){
 	}
 	else {
 	var btrans = "INSERT INTO TransHistory SET ? ";
+	var updatequantity = "UPDATE Product SET Product_quantity = Product_quantity -1 WHERE Product_id = ?";
+	var id = req.params.id;
 	var time =  new Date().toISOString().slice(0, 19).replace('T', ' ');
 	var value = {TransHistory_buyer_id:req.session.user.Customer_id, TransHistory_seller_id: req.session.product.Product_seller_id, 
 			     TransHistory_product_id: req.params.id, TransHistory_type: 1, TransHistory_time: time};
@@ -17,9 +19,16 @@ exports.buy = function(req,res){
 			if(err)
 				res.redirect('/');
 			else{
+				connection.query(updatequantity, id, function(err, result) {
+					if(err)
+						res.redirect('/');
+					else{
 				req.session.product= null;
 				res.send('bought success!');
 				connection.release();
+					}
+				})
+				
 			}
 		});
 	});
